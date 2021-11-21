ebceedb72a6b70fdafb5ce7ec048d1fbf3aed85e
Parvi <sing.parvi1@gmail.com>
2021-11-21 11:56:16 -0500

    Updated transform_pipeline.py to old code for running on GCP

--- transform_pipeline.py
+++ transform_pipeline.py
@@ -40,45 +40,32 @@ def run(argv=None, save_main_session=True):
         required=True,
         help='Output file to write results to.')
     known_args, pipeline_args = parser.parse_known_args(argv)
-    # --Parvi edit
-    # # We use the save_main_session option because one or more DoFn's in this
-    # # workflow rely on global context (e.g., a module imported at module level).
-    # pipeline_options = PipelineOptions(pipeline_args)
-    # pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
-    #
-    # # The pipeline will be run on exiting the with block.
-    # with beam.Pipeline(options=pipeline_options) as p:
-    #     # Read the text file[pattern] into a PCollection.
-    #     lines = p | 'Read' >> ReadFromText(known_args.input)
-    #
-    #     transformandexport = (
-    #             lines | 'GroupBy' >> (beam.GroupBy(lambda s: s[0]))
-    #             | 'Write' >> WriteToText(known_args.output)
-    #     )
-    # --Parvi edit
-    p = beam.Pipeline(options=PipelineOptions())
 
+    # We use the save_main_session option because one or more DoFn's in this
+    # workflow rely on global context (e.g., a module imported at module level).
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
 
-    # class GetTotal(beam.DoFn):
-    #     def process(self, element):
-    #         # get the total transactions for one item
-    #         return [(str(element[0]), sum(element[1]))]
+    # The pipeline will be run on exiting the with block.
+    with beam.Pipeline(options=pipeline_options) as p:
 
-    data_from_source = (p
-                        | 'ReadMyFile' >> ReadFromText(known_args.input)
-                        | 'GroupBy the data' >> beam.GroupByKey()
-                        | 'Export results to new file' >> WriteToText('Results', '.txt')
-                        )
+        # Read the text file[pattern] into a PCollection.
+        lines = p | 'Read' >> ReadFromText(known_args.input)
 
-    # # Format the counts into a PCollection of strings.
-    # def format_result(word, count):
-    #   return '%s: %d' % (word, count)
-    #
-    # output = counts | 'Format' >> beam.MapTuple(format_result)
-    #
-    # # Write the output using a "Write" transform that has side effects.
-    # # pylint: disable=expression-not-assigned
-    # output | 'Write' >> WriteToText(known_args.output)
+        transformandexport = (
+                lines | 'GroupBy' >> beam.GroupByKey()
+                | 'Write' >> WriteToText(known_args.output)
+        )
+
+        # # Format the counts into a PCollection of strings.
+        # def format_result(word, count):
+        #   return '%s: %d' % (word, count)
+        #
+        # output = counts | 'Format' >> beam.MapTuple(format_result)
+        #
+        # # Write the output using a "Write" transform that has side effects.
+        # # pylint: disable=expression-not-assigned
+        # output | 'Write' >> WriteToText(known_args.output)
 
 
 if __name__ == '__main__':
