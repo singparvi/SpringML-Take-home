5c2e7319bf7257afb4030fcede46b2db17aa402a
Parvi <sing.parvi1@gmail.com>
2021-11-20 20:01:25 -0500

    Updated transform_pipeline.py to fix for index issue on GCP

--- transform_pipeline.py
+++ transform_pipeline.py
@@ -27,35 +27,48 @@ from apache_beam.options.pipeline_options import SetupOptions
 
 
 def run(argv=None, save_main_session=True):
-  """Main entry point; defines and runs the wordcount pipeline."""
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--input',
-      dest='input',
-      default='gs://gcp_bucket_springml/Data/AB_NYC_2019.csv',
-      help='Input file to process.')
-  parser.add_argument(
-      '--output',
-      dest='output',
-      required=True,
-      help='Output file to write results to.')
-  known_args, pipeline_args = parser.parse_known_args(argv)
+    """Main entry point; defines and runs the wordcount pipeline."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input',
+        dest='input',
+        default='gs://gcp_bucket_springml/Data/AB_NYC_2019.csv',
+        help='Input file to process.')
+    parser.add_argument(
+        '--output',
+        dest='output',
+        required=True,
+        help='Output file to write results to.')
+    known_args, pipeline_args = parser.parse_known_args(argv)
+    # --Parvi edit
+    # # We use the save_main_session option because one or more DoFn's in this
+    # # workflow rely on global context (e.g., a module imported at module level).
+    # pipeline_options = PipelineOptions(pipeline_args)
+    # pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+    #
+    # # The pipeline will be run on exiting the with block.
+    # with beam.Pipeline(options=pipeline_options) as p:
+    #     # Read the text file[pattern] into a PCollection.
+    #     lines = p | 'Read' >> ReadFromText(known_args.input)
+    #
+    #     transformandexport = (
+    #             lines | 'GroupBy' >> (beam.GroupBy(lambda s: s[0]))
+    #             | 'Write' >> WriteToText(known_args.output)
+    #     )
+    # --Parvi edit
+    p = beam.Pipeline(options=PipelineOptions())
 
-  # We use the save_main_session option because one or more DoFn's in this
-  # workflow rely on global context (e.g., a module imported at module level).
-  pipeline_options = PipelineOptions(pipeline_args)
-  pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
 
-  # The pipeline will be run on exiting the with block.
-  with beam.Pipeline(options=pipeline_options) as p:
+    # class GetTotal(beam.DoFn):
+    #     def process(self, element):
+    #         # get the total transactions for one item
+    #         return [(str(element[0]), sum(element[1]))]
 
-    # Read the text file[pattern] into a PCollection.
-    lines = p | 'Read' >> ReadFromText(known_args.input)
-
-    transformandexport = (
-        lines | 'GroupBy' >> (beam.GroupBy(lambda s: s[0]))
-        | 'Write' >> WriteToText(known_args.output)
-    )
+    data_from_source = (p
+                        | 'ReadMyFile' >> ReadFromText(known_args.input)
+                        | 'GroupBy the data' >> beam.GroupByKey()
+                        | 'Export results to new file' >> WriteToText('Results', '.txt')
+                        )
 
     # # Format the counts into a PCollection of strings.
     # def format_result(word, count):
@@ -69,5 +82,5 @@ def run(argv=None, save_main_session=True):
 
 
 if __name__ == '__main__':
-  logging.getLogger().setLevel(logging.INFO)
-  run()
+    logging.getLogger().setLevel(logging.INFO)
+    run()
